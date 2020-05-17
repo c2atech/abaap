57d1e7f7f0cda544bfa5448140ac8b62400edc42
Alexandre.s <alexandreifto2@gmail.com>
2020-05-17 01:46:52 -0300

    melhorando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,9 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
+               <b> Portal do ADM</b>
                 </a>
-
-
-
+                <a href="{{route('posts')}}">Postagens</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -44,7 +42,6 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                        
-
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
@@ -56,10 +53,8 @@
                         </li>
                         @endif
                         @else
+
                         <li class="nav-item dropdown">
-                        <div mb-3>
-                            <li><a href="{{route('posts')}}">Postagens</a></li>
-                        </div>
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
